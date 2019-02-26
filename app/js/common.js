78d4d02c970d8fea5c1d5372ab65f725058ec8eb
Proton <my.press@bk.ru>
2019-02-26 19:18:27 +0300

    Исправил баг в плагине по отображению выбора кол-ва взрослых, детей и номеров, теперь обработчик событий не дублируются при повторном отображении блока

--- app/js/common.js
+++ app/js/common.js
@@ -1,136 +1,113 @@
 $(function() {
 
-			(function($){
-				$.fn.tool = function( options ) {
-					var settings = $.extend({
-						blockTools: "#visibility",
-						add: ".add",
-						subtract: ".subtract",
-						display: ".display",
-						input: "input",
-						disableButton: "cursor-default",
-						enableButton: "cursor-pointer",
-						postFix: {toggleDisplayChildrens: ["ребенок","ребенка","ребенка","ребенка","детей","детей","детей","детей","детей","детей","детей","детей","детей","детей","детей","детей","детей","детей","детей","детей","ребенок","ребенка","ребенка","ребенка","детей","детей","детей","детей","детей","детей"],
-							toggleDisplayRooms: ["номер","номера","номера","номера","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номер","номера","номера","номера","номеров","номеров","номеров","номеров","номеров","номеров"],
-							toggleDisplayAdults: ["взрослый","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослый","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых"]},
-						preFix: {toggleDisplayChildrens: "&nbsp;·&nbsp;",
-							toggleDisplayRooms: "&nbsp;·&nbsp;"},
-					}, options);
+	(function($){
+		$.fn.tool = function( options ) {
+			var settings = $.extend({
+				blockTools: "#visibility",
+				add: ".add",
+				subtract: ".subtract",
+				display: ".display",
+				input: "input",
+				disableButton: "cursor-default",
+				enableButton: "cursor-pointer",
+				postFix: {toggleDisplayChildrens: ["ребенок","ребенка","ребенка","ребенка","детей","детей","детей","детей","детей","детей","детей","детей","детей","детей","детей","детей","детей","детей","детей","детей","ребенок","ребенка","ребенка","ребенка","детей","детей","детей","детей","детей","детей"],
+					toggleDisplayRooms: ["номер","номера","номера","номера","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номеров","номер","номера","номера","номера","номеров","номеров","номеров","номеров","номеров","номеров"],
+					toggleDisplayAdults: ["взрослый","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослый","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых","взрослых"]},
+				preFix: {toggleDisplayChildrens: "&nbsp;·&nbsp;",
+					toggleDisplayRooms: "&nbsp;·&nbsp;"},
+			}, options),
 
-					var min,max,valueInput,count,target,textDisplay,preFix,postFix;
+			min,max,valueInput,counter,target,textDisplay,preFix,postFix;
 
-					// Не обрататываем событие если кликнули на открвтой форме
-					$(settings.blockTools).click(function(e){
-						e.stopPropagation();
-					});
+			var main = function(selector, typeOperation) {
 
-					// обработали событие клик по ссылке
-					$(this).click(function(e){
-						e.stopPropagation();
-						$(settings.blockTools).removeClass("hidden");
+			var currentCount;
 
-						// обрабатываем событие по клику на +
-						$(settings.blockTools).find(settings.add).click(function() {
+			// Извлекаем из инпута данные для работы с ними дальше
+			min   = parseInt($(selector).siblings(settings.input).prop('min'));
+			max   = parseInt($(selector).siblings(settings.input).prop('max'));
+			valueInput = parseInt($(selector).siblings(settings.input).val());
+			counter = valueInput;
 
-							min   = parseInt($(this).siblings(settings.input).prop('min'));
-							max   = parseInt($(this).siblings(settings.input).prop('max'));
-							valueInput = parseInt($(this).siblings(settings.input).prop('value'));
-							count = valueInput;
-
-							count += 1;
-
-							if ( count == max ) {
-								$(this).prop("disabled", true);
-							}
-
-							$(this).siblings(settings.subtract).prop("disabled", false);
-
-							// если счетчик достиг предела
-							if ( valueInput != max ) {
-								textDisplay = "";
-								preFix      = "";
-								postFix     = "";
-
-								$(this).siblings(settings.display).text(count);
-								$(this).siblings(settings.input).val(count);
-
-								target = $(this).data("target");
-								$.each(settings.postFix, function(index, value) {
-									// если в объекте найден postFix, записываем его в переменную
-									if ( index == target ) {
-										postFix = value[count-1]?
-										value[count-1]:"";
-									}
-
-									$.each(settings.preFix, function(index, value) {
-										// если в объекте найден Prefix записываем его в переменную
-										if ( index == target ) {
-											preFix = value?value:"";
-										}
-									});
-								});
-
-								textDisplay = preFix + "" + count + " " + postFix;
-
-								$("#" + target).html(textDisplay);
-							}
-						});
-
-						$(settings.blockTools).find(settings.subtract).click(function() {
-
-							min   = parseInt($(this).siblings(settings.input).prop('min'));
-							max   = parseInt($(this).siblings(settings.input).prop('max'));
-							valueInput = parseInt($(this).siblings(settings.input).prop('value'));
-							count = valueInput;
-
-							count -= 1;
-
-							if ( count == min ) {
-								$(this).prop("disabled", true);
-							}
-
-							$(this).siblings(settings.add).prop("disabled", false);
-
-							// если счетчик достиг предела
-							if ( valueInput != min ) {
-								textDisplay = "";
-								preFix      = "";
-								postFix     = "";
-
-								textDisplay = "";
-								$(this).siblings(settings.display).text(count);
-								$(this).siblings(settings.input).val(count);
-
-								target = $(this).data("target");
-								$.each(settings.postFix, function(index, value) {
-									// если в объекте найден postFix, записываем его в переменную
-									if ( index == target ) {
-										postFix = value[count-1]?
-										value[count-1]:"";
-									}
-
-									$.each(settings.preFix, function(index, value) {
-										// если в объекте найден Prefix записываем его в переменную
-										if ( index == target ) {
-											preFix = value?value:"";
-										}
-									});
-								});
-
-								if ( count > 0 ) {
-									textDisplay = preFix + "" + count + " " + postFix;
-								}
-								
-								$("#" + target).html(textDisplay);
-							}
-						});
-					});
-
-					$(document).click(function(){
-						$(settings.blockTools).addClass("hidden");
-					});
+				// в зависимости от типа опреации прибавлем или
+				// вычитаем еденицу для счетчика
+				if ( typeOperation == "add" ) {
+					counter++;
+					currentCount = max;
+				} else {
+					counter--;
+					currentCount = min;
 				}
-			})(jQuery);
+
+				// если текущее состояние счетчика
+				// равна минимальной или максимальной
+				// в зависимости от типа операции
+				if ( counter == currentCount ) {
+					// выключаем кнопку (для запрета работы счетчика в эту сторону)
+					$(selector).prop("disabled", true);
+				}
+
+				$(selector).siblings(settings.subtract).prop("disabled", false);
+
+				// если счетчик не достиг предела
+				if ( valueInput != currentCount ) {
+					textDisplay = "";
+					preFix      = "";
+					postFix     = "";
+
+					$(selector).siblings(settings.display).text(counter);
+					$(selector).siblings(settings.input).val(counter);
+
+					target = $(selector).data("target");
+					$.each(settings.postFix, function(index, value) {
+						// если в объекте найден postFix, записываем его в переменную
+						if ( index == target ) {
+							postFix = value[counter-1]?
+							value[counter-1]:"";
+						}
+
+						$.each(settings.preFix, function(index, value) {
+							// если в объекте найден Prefix записываем его в переменную
+							if ( index == target ) {
+								preFix = value?value:"";
+							}
+						});
+					});
+
+					if ( counter > 0 ) {
+						textDisplay = preFix + "" + counter + " " + postFix;
+					}
+					$("#" + target).html(textDisplay);
+				}
+			}; // конец main function
+
+			// Не обрататываем событие если кликнули на открытой форме
+			$(settings.blockTools).click(function(e){
+				e.stopPropagation();
+			});
+
+			// обработали событие клик по ссылке
+			$(this).click(function(e){
+				e.stopPropagation();
+				$(settings.blockTools).removeClass("hidden");
+			});
+
+			// обрабатываем событие по клику на +
+			$(settings.blockTools).find(settings.add).click(function() {
+				main(this, "add");
+			});
+
+			// обрабатываем событие по клику на -
+			$(settings.blockTools).find(settings.subtract).click(function() {
+				main(this, "subtract");
+			});
+
+			$(document).click(function(){
+				$(settings.blockTools).addClass("hidden");
+			});
+		}
+	})(jQuery);
+
 
 	$('#checkin').Zebra_DatePicker({
 		direction: true,
@@ -155,6 +132,7 @@ months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май',
 	// 	return "Are you sure you want to exit? Please complete sign up or the app will get deleted.";
 	// });
 
+
 	$('.owl-carousel').owlCarousel({
 		items: 1,
 		autoplay: true,
@@ -166,8 +144,19 @@ months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май',
 		rewind: true
 	});
 
+
 	$("#guests__toggle").tool( {
 		blockTools: "#option-request",
+		add: ".alubk-stepper__add-button",
+		subtract: ".alubk-stepper__subtract-button",
+		display: ".alubk-stepper__display",
+		input: "input",
+		disableButton: "cursor-default",
+		enableButton: "cursor-pointer",
+	} );
+
+	$("#guests__toggle1").tool( {
+		blockTools: "#option-request1",
 		add: ".alubk-stepper__add-button",
 		subtract: ".alubk-stepper__subtract-button",
 		display: ".alubk-stepper__display",
